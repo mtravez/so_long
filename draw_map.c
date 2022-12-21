adb5e7fe7368bd32c548faf5e0bb9a65782d9aba
Maria Jose Travez <mtravez@student.42heilbronn.de>
2022-12-21 19:55:15 +0100

    Now with grass

--- draw_map.c
+++ draw_map.c
@@ -6,28 +6,52 @@
 /*   By: mtravez <mtravez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 12:22:06 by mtravez           #+#    #+#             */
-/*   Updated: 2022/12/20 13:09:57 by mtravez          ###   ########.fr       */
+/*   Updated: 2022/12/21 17:55:47 by mtravez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#define TILE_WIDTH 35
-#define TILE_LENGTH 35
+#define TILE_WIDTH 32
+#define TILE_LENGTH 32
 
 mlx_image_t *temp_tiles;
 
 void	draw_map(void *mlx, t_map *map)
 {
 	int	i;
+	int j;
 
 	i = 0;
-	mlx_texture_t *texture = mlx_load_png("resources/fence.png");
+	mlx_texture_t *texture = mlx_load_png("resources/grass.png");
 	temp_tiles = mlx_texture_to_image(mlx, texture);
-	while (map->layout[i])
+	while (i < map->length)
 	{
-		mlx_image_to_window(mlx, temp_tiles, i * TILE_WIDTH, 0);
+		j = 0;
+		while (j < map->width)
+		{
+			mlx_image_to_window(mlx, temp_tiles, j * TILE_WIDTH + 10, i * TILE_LENGTH + 10);
+			j++;
+		}
 		i++;
 	}
-		// ft_printf("%i, ", i);
-		// mlx_image_to_window(mlx, temp_tiles, i * TILE_WIDTH, 50);
+	
+	i = 0;
+	texture = mlx_load_png("resources/fence_v.png");
+	temp_tiles = mlx_texture_to_image(mlx, texture);
+	while(i < map->length)
+	{
+		mlx_image_to_window(mlx, temp_tiles, 10, i * TILE_LENGTH);
+		mlx_image_to_window(mlx, temp_tiles, map->width * TILE_WIDTH, i * TILE_LENGTH) ;
+		i++;
+	}
+	
+	i = 0;
+	texture = mlx_load_png("resources/fence.png");
+	temp_tiles = mlx_texture_to_image(mlx, texture);
+	while(i < map->width)
+	{
+		mlx_image_to_window(mlx, temp_tiles, i * TILE_WIDTH + 10, 0);
+		mlx_image_to_window(mlx, temp_tiles, i * TILE_WIDTH + 10, map->length * TILE_LENGTH);
+		i++;
+	}
 }
--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: mtravez <mtravez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/06 21:29:56 by mtravez           #+#    #+#             */
-/*   Updated: 2022/12/20 18:45:53 by mtravez          ###   ########.fr       */
+/*   Updated: 2022/12/21 19:37:07 by mtravez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ void hook(void *param)
 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(mlx);
 	if (mlx_is_key_down(mlx, MLX_KEY_W))
-		g_img->instances[0].y -= 15;
+		g_img->instances[0].y -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_S))
-		g_img->instances[0].y += 15;
+		g_img->instances[0].y += 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_A))
-		g_img->instances[0].x -= 15;
+		g_img->instances[0].x -= 5;
 	if (mlx_is_key_down(mlx, MLX_KEY_D))
-		g_img->instances[0].x += 15;
+		g_img->instances[0].x += 5; 
 		
 }
 
@@ -58,7 +58,7 @@ void move(void *param)
 	
 	if (mlx_is_key_down(mlx, MLX_KEY_SPACE))
 	{
-		texture = mlx_load_png("resources/bicho5.png");
+		texture = mlx_load_png("resources/bicho.png");
 		mlx_draw_texture(g_img, texture, 10, 10);
 		// texture = mlx_load_png("resources/bicho3.png");
 		// mlx_draw_texture(g_img, texture, g_img->instances[0].x, g_img->instances[0].y);
@@ -114,41 +114,35 @@ int main(int argc, char **argv)
 	// getchar();
 	// ft_printf("hello");
 	// draw_map(mlx, map);
-	texture = mlx_load_png("resources/fence.png");
-	// ft_printf("%i, ", map->width);
-	mlx_image_t *tiles = mlx_texture_to_image(mlx, texture);
-	int i = 0;
-	while(i < map->width)
-	{
-		mlx_image_to_window(mlx, tiles, i * 32, 0);
-		mlx_image_to_window(mlx, tiles, i * 32, map->length * 32);
-		i++;
-	}
+	// texture = mlx_load_png("resources/fence.png");
+	// // ft_printf("%i, ", map->width);
+	// mlx_image_t *tiles = mlx_texture_to_image(mlx, texture);
+	// int i = 0;
+	// while(i < map->width)
+	// {
+	// 	mlx_image_to_window(mlx, tiles, i * 32, 0);
+	// 	mlx_image_to_window(mlx, tiles, i * 32, map->length * 32);
+	// 	i++;
+	// }
 
-	i = 1;
-	texture = mlx_load_png("resources/fence_v.png");
-	tiles = mlx_texture_to_image(mlx, texture);
-	while(i < map->length - 1)
-	{
-		mlx_image_to_window(mlx, tiles, 0, (i * 45) - 25);
-		mlx_image_to_window(mlx, tiles, map->width * 31, (i * 45) - 25);
-		i++;
-	}
-	// draw_map(mlx, map);
+	// i = 1;
+	// texture = mlx_load_png("resources/fence_v.png");
+	// tiles = mlx_texture_to_image(mlx, texture);
+	// while(i < map->length - 1)
+	// {
+	// 	mlx_image_to_window(mlx, tiles, 0, (i * 45) - 25);
+	// 	mlx_image_to_window(mlx, tiles, map->width * 31, (i * 45) - 25);
+	// 	i++;
+	// }
+	draw_map(mlx, map);
 	
 	
 	// Zorrito
-	// texture = mlx_load_png("resources/bicho.png");
-	// g_img = mlx_texture_to_image(mlx, texture);
-	// ft_printf("height: %i, width: %i", g_img->height, g_img->width);
-	// mlx_key_data_t *hi = malloc(sizeof(mlx_key_data_t))  ;
-	// hi->action = MLX_RELEASE;
-	// hi->key = MLX_KEY_D;
-	// mlx_keyfunc func(hi, mlx);
-	// mlx_image_to_window(mlx, g_img, 0, 0);
-	// mlx_loop_hook(mlx, &hook, mlx);
-	// mlx_loop_hook(mlx, &move, mlx);
-	// mlx_key_hook(mlx, func, mlx);
+	texture = mlx_load_png("resources/bicho.png");
+	g_img = mlx_texture_to_image(mlx, texture);
+	mlx_image_to_window(mlx, g_img, 0, 0);
+	mlx_loop_hook(mlx, &hook, mlx);
+	mlx_loop_hook(mlx, &move, mlx);
 	
 	// Fence
 	// texture = mlx_load_png("resources/fence.png");
